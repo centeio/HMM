4fa7a54a638f8d325476788d505aae3cdd7c5654
Higor Araújo dos Anjos <higor.araujo.anjos@gmail.com>
2018-05-19 23:30:46 +0100

    HMM for E.Coli taking too long

--- hmm.py
+++ hmm.py
@@ -1,37 +1,42 @@
-
+from util import io
+from trainer import Trainer
 
 class Model():
 
     def __init__(self):
         self.states = ['0','1','2','3','4','5','6','7','8','9']
         self.symbols = ['A','C','G','T']
-        self._trans_dict = {'0': {'0': 0.3,'1': 0, '2': 0.35,'3': 0.35, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0},
-                            '1': {'0': 0.3,'1': 0, '2': 0.35,'3': 0.35, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0},
-                            '2': {'0': 0.3,'1': 0, '2': 0.35,'3': 0.35, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0},
-                            '3': {'0': 0.3,'1': 0, '2': 0.35,'3': 0.35, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0},
-                            '4': {'0': 0.3,'1': 0, '2': 0.35,'3': 0.35, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0},
-                            '5': {'0': 0.3,'1': 0, '2': 0.35,'3': 0.35, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0},
-                            '6': {'0': 0.3,'1': 0, '2': 0.35,'3': 0.35, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0},
-                            '7': {'0': 0.3,'1': 0, '2': 0.35,'3': 0.35, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0},
-                            '8': {'0': 0.3,'1': 0, '2': 0.35,'3': 0.35, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0},
-                            '9': {'0': 0.3,'1': 0, '2': 0.35,'3': 0.35, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0}
-                            }
-        self._emit_dict = {'0': {'A': 0.25, 'C':0.25, 'G':0.25, 'T': 0.25}, 
-                            '1': {'A': 0.25, 'C': 0.25, 'G': 0.25, 'T': 0.25}, 
-                            '2': {'A': 0.25, 'C': 0.25, 'G': 0.25, 'T': 0.25},
-                            '3': {'A': 0.25, 'C': 0.25, 'G': 0.25, 'T': 0.25},
-                            '4': {'A': 0.25, 'C': 0.25, 'G': 0.25, 'T': 0.25},
-                            '5': {'A': 0.25, 'C': 0.25, 'G': 0.25, 'T': 0.25},
-                            '6': {'A': 0.25, 'C': 0.25, 'G': 0.25, 'T': 0.25},
-                            '7': {'A': 0.25, 'C': 0.25, 'G': 0.25, 'T': 0.25},
-                            '8': {'A': 0.25, 'C': 0.25, 'G': 0.25, 'T': 0.25},
-                            '9': {'A': 0.25, 'C': 0.25, 'G': 0.25, 'T': 0.25}}
+        self._emit_dict = {
+                    0:{(' ','A'):1,(' ','C'):1,(' ','G'):1,(' ','T'):1,('A','A'):1,('A','C'):1,('A','G'):1,('A','T'):1,('C','A'):1,('C','C'):1,('C','G'):1,('C','T'):1,('G','A'):1,('G','C'):1,('G','G'):1,('G','T'):1,('T','A'):1,('T','C'):1,('T','G'):1,('T','T'):1,"total": 20},
+                    1:{('A','A'):1,('A','C'):1,('A','G'):1,('A','T'):1,('C','A'):1,('C','C'):1,('C','G'):1,('C','T'):1,('G','A'):1,('G','C'):1,('G','G'):1,('G','T'):1,('T','A'):1,('T','C'):1,('T','G'):1,('T','T'):1,"total":16},
+                    2:{('A','A'):1,('A','C'):1,('A','G'):1,('A','T'):1,('C','A'):1,('C','C'):1,('C','G'):1,('C','T'):1,('G','A'):1,('G','C'):1,('G','G'):1,('G','T'):1,('T','A'):1,('T','C'):1,('T','G'):1,('T','T'):1,"total":16},
+                    3:{('A','A'):1,('A','C'):1,('A','G'):1,('A','T'):1,('C','A'):1,('C','C'):1,('C','G'):1,('C','T'):1,('G','A'):1,('G','C'):1,('G','G'):1,('G','T'):1,('T','A'):1,('T','C'):1,('T','G'):1,('T','T'):1,"total":16},
+                    4:{('A','A'):1,('A','C'):1,('A','G'):1,('A','T'):1,('C','A'):1,('C','C'):1,('C','G'):1,('C','T'):1,('G','A'):1,('G','C'):1,('G','G'):1,('G','T'):1,('T','A'):1,('T','C'):1,('T','G'):1,('T','T'):1,"total":16},
+                    5:{('A','A'):1,('A','C'):1,('A','G'):1,('A','T'):1,('C','A'):1,('C','C'):1,('C','G'):1,('C','T'):1,('G','A'):1,('G','C'):1,('G','G'):1,('G','T'):1,('T','A'):1,('T','C'):1,('T','G'):1,('T','T'):1,"total":16},
+                    6:{('A','A'):1,('A','C'):1,('A','G'):1,('A','T'):1,('C','A'):1,('C','C'):1,('C','G'):1,('C','T'):1,('G','A'):1,('G','C'):1,('G','G'):1,('G','T'):1,('T','A'):1,('T','C'):1,('T','G'):1,('T','T'):1,"total":16},
+                    7:{('A','A'):1,('A','C'):1,('A','G'):1,('A','T'):1,('C','A'):1,('C','C'):1,('C','G'):1,('C','T'):1,('G','A'):1,('G','C'):1,('G','G'):1,('G','T'):1,('T','A'):1,('T','C'):1,('T','G'):1,('T','T'):1,"total":16},
+                    8:{('A','A'):1,('A','C'):1,('A','G'):1,('A','T'):1,('C','A'):1,('C','C'):1,('C','G'):1,('C','T'):1,('G','A'):1,('G','C'):1,('G','G'):1,('G','T'):1,('T','A'):1,('T','C'):1,('T','G'):1,('T','T'):1,"total":16},
+                    9:{('A','A'):1,('A','C'):1,('A','G'):1,('A','T'):1,('C','A'):1,('C','C'):1,('C','G'):1,('C','T'):1,('G','A'):1,('G','C'):1,('G','G'):1,('G','T'):1,('T','A'):1,('T','C'):1,('T','G'):1,('T','T'):1,"total":16}
+                    }
+
+        self._trans_dict = {
+                    '0':{'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0},
+                    '1':{'0':0,'1':0,'2':1,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0},
+                    '2':{'0':0,'1':0,'2':0,'3':1,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0},
+                    '3':{'0':0,'1':0,'2':0,'3':0,'4':1,'5':0,'6':0,'7':0,'8':0,'9':0},
+                    '4':{'0':0,'1':0,'2':0,'3':0,'4':0,'5':1,'6':0,'7':0,'8':0,'9':0},
+                    '5':{'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':1,'7':0,'8':0,'9':0},
+                    '6':{'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0},
+                    '7':{'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':1,'9':0},
+                    '8':{'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':1},
+                    '9':{'0':1,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
+                    }
 
         self._init_p = {'0':1,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
 
-    def prob_emit(self, state, symbol):
+    def prob_emit(self, state, current, prev=' '):
         #TODO train
-        return self._emit_dict[state][symbol]
+        return self._emit_dict[int(state)][(prev,current)]
 
     def prob_trans(self, fromstate, tostate):
         #TODO train
@@ -49,19 +54,28 @@ class Model():
         path = {}     
         # Initialize
         for y in self.states:
-            vit[0][y] = self._init_p[y] * self._emit_dict[y][obs[0]]
+            vit[0][y] = self._init_p[y] * self.prob_emit(y,current=obs[0])
             path[y] = [y]
+        print("obs",obs[0])
+        print("vit",vit)
+        print("path", path)
 
-        print(vit)
         for t in range(1, len(obs)):
             vit.append({})
-            newpath = {}     
+            newpath = {}
+            print("T",t,"/",len(obs))        
             for y in self.states:
-                (prob, state) = max((vit[t-1][y0] * self._trans_dict[y0][y] * self._emit_dict[y][obs[t]], y0) for y0 in self.states)
+                test = [(vit[t-1][y0] * self.prob_trans(fromstate=y0, tostate=y) * self.prob_emit(state=y,prev=obs[t-1],current=obs[t]), y0) for y0 in self.states]
+                print("TESTE",test)
+                (prob, state) = max(test)
                 vit[t][y] = prob
                 newpath[y] = path[state] + [y]     
 
             path = newpath
+            print("obs",obs[0])
+            print("vit",vit)
+            print("path", path)
+            return
         n = 0
         if len(obs)!=1:
             n = t
@@ -72,22 +86,45 @@ class Model():
         fwd = [{}]     
         # t == 0
         for y in self.states:
-            fwd[0][y] = self._init_p[y] * self._emit_dict[y][obs[0]]
+            fwd[0][y] = self._init_p[y] * self.prob_emit(state=y,current=obs[0])
         # t > 0
+        
         for t in range(1, len(obs)):
-            fwd.append({})     
+            fwd.append({})
+            print("T",t,"/",len(obs))     
             for y in self.states:
-                fwd[t][y] = sum((fwd[t-1][y0] * self._trans_dict[y0][y] * self._emit_dict[y][obs[t]]) for y0 in self.states)
+                fwd[t][y] = sum((fwd[t-1][y0] * self.prob_trans(fromstate=y0, tostate=y) * self.prob_emit(state=y,prev=obs[t-1], current=obs[t])) for y0 in self.states)
+        
         prob = sum((fwd[len(obs) - 1][s]) for s in self.states)
         return prob
-
     
+    def set_emit_dict(self, dictionary):
+        self._emit_dict = dictionary
+
+    def set_trans_dict(self, dictionary):
+        self._trans_dict = dictionary
 
 
 if __name__ == "__main__":
+    #train
+    print("Training...")
+    # t = Trainer()
+    # t.train("./NC_000913_3.fasta", "./NC_000913_3.gb")
+    # t.persist("./trained.p")
+    t = Trainer.retrieve("./trained.p")
+    #create model
     m = Model()
-    prob, path = m.viterbi('AAGT')
+    #setting trained info
+    m.set_trans_dict(t.get_trans_dict())
+    m.set_emit_dict(t.get_emit_dict())
+    coli = io.read_fasta(io.read_file("./NC_000913_3.fasta"))
+    #coli = "ACTAGACAAAATC"
+    coli = coli[:int(len(coli)/10)]
+    print("Viterbi...")
+    prob, path = m.viterbi(coli)
     print(prob, path)
-
-    p = m.forward('AAGT')
+    
+    print("Forward...")
+    p = m.forward(coli)
     print(p)
+
